9bb1990c42014b9b05545baae7d2d512166d920d
SunTheCoder <sunenglishjr@gmail.com>
2024-08-01 18:58:26 -0400

    eventlistener&functionality fix

--- game.js
+++ game.js
@@ -8,6 +8,12 @@ window.addEventListener('DOMContentLoaded', () => {
     const kitty1 = document.getElementById('kitty-1')
     const kitty2 = document.getElementById('kitty-2')
 
+    const eyes1 = document.getElementById('eyes-1')
+    const eyes1kitty = document.getElementById('eyes-1-kitty')
+    const eyes2 = document.getElementById('eyes-2')
+    const eyes2kitty = document.getElementById('eyes-2-kitty')
+
+
     // panel1.addEventListener('click', () => {
     //     head1.style.display = 'none'
     // })
@@ -20,5 +26,19 @@ window.addEventListener('DOMContentLoaded', () => {
     //     kitty1.style.display = 'block'
     //     kitty2.style.display = 'none'
     // })
+    eyes1.addEventListener('click', () => {
+        
+        panel1.innerHTML =`<img id="kitty-1" class="body" src="/Pensive-Kitty-Blank.png" alt="">
+             
+        <img id="eyes-1-kitty" class="body-button" src="/Pensive-Kitty-Eyes.png" alt="">`
+        
+
+    })
+    eyes2.addEventListener('click', () => {
+        panel1.innerHTML =`<img id="kitty-1" class="body" src="/Pensive-Kitty-Blank.png" alt="">
+             
+        <img id="eyes-2-kitty" class="body-button" src="/Pensive-Kitty-Eyes-v2.png" alt="">`
+
+    })
 
 })
