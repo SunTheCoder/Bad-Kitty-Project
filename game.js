6329b1a222fc99f401b927c565e133c9718cd9c9
SunTheCoder <sunenglishjr@gmail.com>
2024-08-01 13:53:54 -0400

    new asset created

--- game.js
+++ game.js
@@ -5,18 +5,20 @@ window.addEventListener('DOMContentLoaded', () => {
     const head1button = document.getElementById('head-1-button')
     const head2button = document.getElementById('head-2-button')
     const head2 = document.getElementById('head-2')
+    const kitty1 = document.getElementById('kitty-1')
+    const kitty2 = document.getElementById('kitty-2')
 
     // panel1.addEventListener('click', () => {
     //     head1.style.display = 'none'
     // })
 
-    head2button.addEventListener('click', () => {
-        head2.style.display = 'block'
-        head1.style.display = 'none'
-    })
-    head1button.addEventListener('click', () => {
-        head1.style.display = 'block'
-        head2.style.display = 'none'
-    })
+    // head2button.addEventListener('click', () => {
+    //     kitty2.style.display = 'block'
+    //     kitty1.style.display = 'none'
+    // })
+    // head1button.addEventListener('click', () => {
+    //     kitty1.style.display = 'block'
+    //     kitty2.style.display = 'none'
+    // })
 
 })
