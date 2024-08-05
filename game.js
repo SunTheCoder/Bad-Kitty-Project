f13ecae94193d39d5e23a79f91eb27d064ae053f
SunTheCoder <sunenglishjr@gmail.com>
2024-08-05 17:51:38 -0400

    eye4 functionality

--- game.js
+++ game.js
@@ -13,6 +13,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const eyes2 = document.getElementById('eyes-2')
     const eyes2kitty = document.getElementById('eyes-2-kitty')
     const eyes3 = document.getElementById('eyes-3')
+    const eyes4 = document.getElementById('eyes-4')
+    
 
 
 
@@ -55,6 +57,16 @@ window.addEventListener('DOMContentLoaded', () => {
         <img src="/Assets/Thought-Bubble-1.png" alt="" id="thought-bubble-1" class="speech-bubble">
         
         <img id="eyes-3-kitty" class="body-button" src="/Assets/Pensive-Kitty-Eyes-Closed-White.png" alt="">
+        <img src="/Assets/kitty-treat-bag.png" alt="" id="treat-thought">`
+
+
+    })
+    eyes4.addEventListener('click', () => {
+        panel1.innerHTML =`<img id="kitty-1" class="body" src="/Assets/Pensive-Kitty-blank-v2.png" alt="">
+
+        <img src="/Assets/Thought-Bubble-1.png" alt="" id="thought-bubble-1" class="speech-bubble">
+        
+        <img id="eyes-4-kitty" class="body-button" src="/Assets/Pensive-Kitty-devious-white.png" alt="">
         <img src="/Assets/kitty-treat-bag.png" alt="" id="treat-thought">`
 
 
